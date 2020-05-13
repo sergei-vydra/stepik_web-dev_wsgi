99ae432a37c95b707d0c24e357ff516297a1eb9f
sergei_vydra <vydrasergei@gmail.com>
2020-05-13 12:20:37 +0300

    added configs, wsgi-hello.py

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/wsgi.example
