852bed9de0c883269a811083970d9f97c5d5b7bc
sergei_vydra <vydrasergei@gmail.com>
2020-05-13 12:18:06 +0300

    added configs, wsgi-hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def wsgi_app(environ, start_response):
     status = "200 OK"
-    headers = [{"Content-Type": "text/plain"}]
+    headers = [('Content-Type', 'text/plain')]
     body = "\r\n".join(environ["QUERY_STRING"].split("&"))
     start_response(status, headers)
     return [bytes(body, encoding="utf-8")]
